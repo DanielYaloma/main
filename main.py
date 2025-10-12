0dc612783ee1b4f30376911f6d07aef0b6bfc2a2
Ялома Даниэль <stud0000347205@utmn.ru>
2025-10-12 17:42:50 +0500

    Високосный год

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+number = 212122
+digits = str(number)
+if len(digits) == 6:
+    if sum(map(int, digits[:3])) == sum(map(int, digits[3:])):
+        print("Счастливый билет")
+    else:
+        print("Несчастливый билет")
+else:
+    print("Введите шестизначное число.")
