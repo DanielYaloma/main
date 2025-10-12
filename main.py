59dcff51d32db8a98ef2f1d429460883994359ba
Ялома Даниэль <stud0000347205@utmn.ru>
2025-10-12 17:04:04 +0500

    Високосный/Обычный год ДЗ

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-msc_price = 13_000
-spb_price = 10_000
-ekb_price = 8_000
+year = 2024
 
-dist = input("Введите пункт поездки (msc, spb, ekb): ")
-adults = int(input("Введите количество взрослых: "))
-kids = int(input("Введите количество детей: "))
-
-if dist == "msc":
-    dist_price = msc_price
-elif dist == "spb":
-    dist_price = spb_price
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print("Високосный год")
 else:
-    dist_price = ekb_price
-
-total = dist_price * (2 * adults + kids) // 2
-
-print("Цена поездки", total)
+    print("Обычный год")
