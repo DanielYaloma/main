246a66dbd22a67e43454b59b3a76880c379150ae
DanielYaloma <stud0000347205@utmn.ru>
2025-11-02 23:41:32 +0500

    documents = [     {'type': 'passport', 'number': '2207 876234', 'name': 'Василий Гупкин'},     {'type': 'invoice', 'number': '11-2', 'name': 'Геннадий Покемонов'},     {'type': 'insurance', 'number': '10006', 'name': 'Аристарх Павлов'} ] directories = {     '1': ['2207 876234', '11-2'],     '2': ['10006'],     '3': [] }
    # создал словарь, где ключи это номера полок,а значения это списки номеров документа.
    def find_owner(doc_number, docs):
        for doc in docs:
            if doc['number'] == doc_number:
                return doc['name']
        return None
    
    # бесконечный цикл для ввода команд, ищет владельца документа по номеру или выходил по команде 'q'
    while True:
        command = input("Введите команду:\n").strip().lower()
        if command == 'p':
            number = input("Введите номер документа:\n").strip()
            owner = find_owner(number, documents)
            if owner:
                print(f"Владелец документа: {owner}")
            else:
                print("Документ не найден.")
        elif command == 'q':
            break
    
    # Незнаю что еще в комментариях написать, но вроде описал что делал)

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-year = 2024
+documents = [
+    {'type': 'passport', 'number': '2207 876234', 'name': 'Василий Гупкин'},
+    {'type': 'invoice', 'number': '11-2', 'name': 'Геннадий Покемонов'},
+    {'type': 'insurance', 'number': '10006', 'name': 'Аристарх Павлов'}
+]
+directories = {
+    '1': ['2207 876234', '11-2'],
+    '2': ['10006'],
+    '3': []
+}
 
-if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-    print("Високосный год")
-else:
-    print("Обычный год")
+# создал словарь, где ключи это номера полок,а значения это списки номеров документа.
+def find_owner(doc_number, docs):
+    for doc in docs:
+        if doc['number'] == doc_number:
+            return doc['name']
+    return None
+
+# бесконечный цикл для ввода команд, ищет владельца документа по номеру или выходил по команде 'q'
+while True:
+    command = input("Введите команду:\n").strip().lower()
+    if command == 'p':
+        number = input("Введите номер документа:\n").strip()
+        owner = find_owner(number, documents)
+        if owner:
+            print(f"Владелец документа: {owner}")
+        else:
+            print("Документ не найден.")
+    elif command == 'q':
+        break
+
+# Незнаю что еще в комментариях написать, но вроде описал что делал)
